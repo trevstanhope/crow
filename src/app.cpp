e5c5dd537018f58016f9a391ea75e9dd7a3e2826
trevor <trevor@debian>
2015-10-17 23:51:01 -0400

    added serial.h and next step is to run a task scheduler

--- src/app.cpp
+++ src/app.cpp
@@ -3,8 +3,12 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <sstream>
+#include <iostream>
+#include "serial.h"
 
 using namespace cv;
+using namespace std;
+using namespace boost;
 
 class ExampleLogHandler : public crow::ILogHandler {
     public:
@@ -20,84 +24,79 @@ struct ExampleMiddleware {
         message = "foo";
     }
 
-    void setMessage(std::string newMsg)
-    {
-        message = newMsg;
-    }
+    void setMessage(std::string newMsg) { message = newMsg; }
 
-    struct context
-    {
-    };
+    struct context {};
 
-    void before_handle(crow::request& req, crow::response& res, context& ctx)
-    {
+    void before_handle(crow::request &req, crow::response &res, context &ctx) {
         CROW_LOG_DEBUG << " - MESSAGE: " << message;
     }
 
-    void after_handle(crow::request& req, crow::response& res, context& ctx)
-    {
+    void after_handle(crow::request &req, crow::response &res, context &ctx) {
         // no-op
     }
 };
 
 int main(int argc, char** argv ) {
 
-    VideoCapture cap(0); // capture the video from web cam
-    if ( !cap.isOpened() ) {  // if not success, exit program
-         return -1;
-    }
+  // Serial Device
+  string port = argv[1];    // the device path
+  int baud = atoi(argv[2]); // parse string to integer
+  try {
+    Serial serial(port, baud);
+    serial.writeString("Hello world\n");
+    cout << serial.readLine() << endl;
+  } catch (boost::system::system_error &e) {
+    cout << "Error: " << e.what() << endl;
+    return 1;
+  }
 
-    Mat imgBGR;
-    bool bSuccess = cap.read(imgBGR);
-    if (bSuccess) {
-        Mat imgHSV;
-        cvtColor(imgBGR, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-    }
-    else {
-        return -1;
-    }
+  // Video Stream
+  VideoCapture cap(0);        // capture the video from web cam
+  if (!cap.isOpened()) {      // if not success, exit program
+    return -1;
+  }
 
-    crow::App<ExampleMiddleware> app;
+  Mat imgBGR;
+  bool bSuccess = cap.read(imgBGR);
+  if (bSuccess) {
+    Mat imgHSV;
+    cvtColor(imgBGR, imgHSV,
+             COLOR_BGR2HSV); // Convert the captured frame from BGR to HSV
+  } else {
+    return -1;
+  }
 
-    app.get_middleware<ExampleMiddleware>().setMessage("hello");
+  // Webserver
+  crow::App<ExampleMiddleware> app;
+  app.get_middleware<ExampleMiddleware>().setMessage("hello");
+  CROW_ROUTE(app, "/").name("hello")([] { return "Hello World!"; });
+  CROW_ROUTE(app, "/about")
+  ([]() { return "About Crow example."; });
 
-    CROW_ROUTE(app, "/")
-        .name("hello")
-    ([]{
-        return "Hello World!";
-    });
+  // simple json response
+  // To see it in action enter {ip}:18080/json
+  CROW_ROUTE(app, "/config")
+  ([] {
+    crow::json::wvalue x;
+    x["message"] = "Hello, World!";
+    return x;
+  });
 
-    CROW_ROUTE(app, "/about")
-    ([](){
-        return "About Crow example.";
-    });
-
-    // simple json response
-    // To see it in action enter {ip}:18080/json
-    CROW_ROUTE(app, "/config")
-    ([]{
-        crow::json::wvalue x;
-        x["message"] = "Hello, World!";
-        return x;
-    });
-
-    CROW_ROUTE(app, "/update")
-        .methods("POST"_method)
-    ([](const crow::request& req){
+  CROW_ROUTE(app, "/update")
+      .methods("POST"_method)([](const crow::request &req) {
         auto x = crow::json::load(req.body);
         if (!x)
-            return crow::response(400);
+          return crow::response(400);
         int sum = x["a"].i()+x["b"].i();
         std::ostringstream os;
         os << sum;
         return crow::response{os.str()};
-    });
+      });
 
-    // ignore all log
-    crow::logger::setLogLevel(crow::LogLevel::DEBUG);
-    //crow::logger::setHandler(std::make_shared<ExampleLogHandler>());
+  // ignore all log
+  crow::logger::setLogLevel(crow::LogLevel::DEBUG);
+  // crow::logger::setHandler(std::make_shared<ExampleLogHandler>());
 
-    app.port(18080)
-        .multithreaded()
-        .run();
+  app.port(18080).multithreaded().run();
 }
